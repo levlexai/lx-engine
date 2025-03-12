1e89a37446d3eea1643d521d01ee7715409ffa56
twizzworld <103385201+numinousmuses@users.noreply.github.com>
2025-03-12 13:15:37 -0500

    finished v0.9

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { initTableManager } from './utils/tableManager';
 import { initNotebookManager } from './utils/notebookManager';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
